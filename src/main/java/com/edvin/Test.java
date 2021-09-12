f438bb26e1c7960e96c4641c3bdff3ec5a864dc3
edvin <e.tuzlakovic@free.fr>
2021-09-12 13:41:33 +0200

    Comments

--- src/main/java/com/edvin/Test.java
+++ src/main/java/com/edvin/Test.java
@@ -1,8 +1,13 @@
 package com.edvin;
 
+/**
+    Comment
+ */
+
 public class Test {
 
     public static void main(String[] args) {
+        /* Comment dev */
         System.out.println("Hello World!");
     }
 }
